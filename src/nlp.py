1facc1cd40f9e8fd2d1f6c9ab229f3957f70f3f1
Pedro Elorriaga <pedrohes2002@outlook.com>
2025-04-11 11:29:00 -0300

    Feat 11/04/25: Scraping an AI handle with BERT models for comments

---
+++ src/nlp.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import re
+import nltk
+from unidecode import unidecode
+from transformers import pipeline
+
+# SETTING NLTK (Natural Language Toolkit) AND DONWLOADING STOPWORDS (é, o, de, a)
+nltk.download('stopwords')
+stopwords = set(nltk.corpus.stopwords.words('portuguese'))
+
+
+# CLEAN TEXT FUNCTION
+def clean_text(text: str):
+    text = unidecode(text.lower())
+    text = re.sub(r'\d+', '', text)
+    text = re.sub(r'[^\w\s]', '', text)
+    words = text.split()
+    words = [w for w in words if w not in stopwords]
+    return ' '.join(words)
+
+
+# MODEL PRE LOAD
+model = pipeline('sentiment-analysis',
+                 model='nlptown/bert-base-multilingual-uncased-sentiment')
+
+
+# HANDLE USING BERT MODEL
+def feeling_handle(text: str):
+    try:
+        result = model(text[:512])
+        stars = int(result[0]['label'].split(' ')[0])
+
+        if stars <= 2:
+            return 'NEGATIVO'
+        elif stars == 3:
+            return 'NEUTRO'
+        else:
+            return 'POSITIVO'
+
+    except:
+        return 'Error'
+
+
+df_comments = pd.read_csv(
+    r'E:\Projetos-Codes\AI-ReclamaEm\data\raw\reclamacoes.csv')
+df_comments['texto_limpo'] = df_comments['conteudo'].apply(
+    clean_text)  # apply usado para aplicar função no pandas
+df_comments['sentimento'] = df_comments['texto_limpo'].apply(feeling_handle)
+df_comments.to_csv('data/processed/reclamacoes_processado.csv', index=False,)
---
+++ src/scraping.py
@@ -0,0 +1,49 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+import pandas as pd
+
+# SETTING WEBDRIVER
+options = webdriver.ChromeOptions()
+options.add_experimental_option('detach', True)
+driver = webdriver.Chrome(options)
+
+
+# GETTING PAGE
+driver.get('https://www.reclameaqui.com.br/empresa/nubank/lista-reclamacoes/')
+
+# GET ALL COMMENTS (ONLY ONE SECTION)
+contanier_comment = driver.find_element(
+    By.XPATH, '//*[@id="__next"]/div[3]/div/div/main/section[2]/div[2]/div[2]')
+div_comments_array = contanier_comment.find_elements(
+    By.CLASS_NAME, 'sc-1pe7b5t-0')
+
+# HANDLING EACH COMMENT
+comments_object = []
+for comment in div_comments_array:
+    a_title_comment = comment.find_element(By.TAG_NAME, 'a')
+    p_content_comment = comment.find_element(By.TAG_NAME, 'p')
+    span_contents_comment = comment.find_elements(By.TAG_NAME, 'span')
+
+    # SCROLL TO COMMENT
+    driver.execute_script(
+        "arguments[0].scrollIntoView({ behavior: 'smooth', block: 'center' });", a_title_comment)
+    time.sleep(1.5)
+    comments_object.append(
+        {
+            "titulo": a_title_comment.text,
+            "conteudo": p_content_comment.text,
+            "resposta": False if span_contents_comment[0].text == 'Não respondida' else True,
+            "data": span_contents_comment[1].text
+        }
+    )
+
+# HANDLE WITH DATAFRAMES
+df_comments = pd.DataFrame(comments_object)
+df_comments.to_csv('data/raw/reclamacoes.csv',
+                   index=False, encoding='utf-8')
+
+driver.close()
+driver.quit()
